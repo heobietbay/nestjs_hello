3bebb6e0b8991d42c5ebf82dd39f32db167e9d6a
Tran Dang Khoa <tetsusoiwashiro@gmail.com>
2023-02-21 17:42:42 +0700

    update prisma implement constraint handling

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
@@ -11,15 +12,31 @@ export class AuthService {
     // generate password hash
     const hash = await argon.hash(dto.password);
 
-    // save user to database
-    const user = await this.prismaService.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-
-    return user;
+    try {
+      // save user to database
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        }
+      });
+  
+      return user;
+    }
+    catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === "P2002") {
+          // duplicate user error
+          throw new ForbiddenException("User already exists");
+        }
+      }
+      throw err;
+    }
   }
   signin() {
     return "signin";
